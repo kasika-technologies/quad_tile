392aeb282e89b8994361ff5766d62f87f47bcf70
SAKAIDA Atsushi <sakaida@musubu.co.jp>
2019-09-04 17:53:56 +0900

    implemented TileForPoint, TilesForArea

---
+++ quad_tile.go
@@ -0,0 +1,56 @@
+package quad_tile
+
+import (
+	"math"
+	"sort"
+)
+
+func TileForPoint(longitude, latitude float64) uint64 {
+	x := lon2X(longitude)
+	y := lat2Y(latitude)
+
+	return xy2Tile(x, y)
+}
+
+func TilesForArea(minLongitude, minLatitude, maxLongitude, maxLatitude float64) []uint64 {
+	minX := lon2X(minLongitude)
+	maxX := lon2X(maxLongitude)
+	minY := lat2Y(minLatitude)
+	maxY := lat2Y(maxLatitude)
+
+	var tiles []uint64
+
+	for x := minX; x <= maxX; x++ {
+		for y := minY; y <= maxY; y++ {
+			tile := xy2Tile(x, y)
+			tiles = append(tiles, tile)
+		}
+	}
+
+	sort.Slice(tiles, func(i, j int) bool {
+		return tiles[i] < tiles[j]
+	})
+
+	return tiles
+}
+
+func lon2X(lon float64) uint64 {
+	n := math.Round((lon + 180.0) * 65535.0 / 360.0)
+	return uint64(n + 0.5)
+}
+
+func lat2Y(lat float64) uint64 {
+	n := math.Round((lat + 90.0) * 65535.0 / 180.0)
+	return uint64(n + 0.5)
+}
+
+func xy2Tile(x, y uint64) uint64 {
+	tile := uint64(0)
+
+	for i := 15; i >= 0; i-- {
+		tile = (tile << 1) | ((x >> i) & 1)
+		tile = (tile << 1) | ((y >> i) & 1)
+	}
+
+	return tile
+}
---
+++ quad_tile_test.go
@@ -0,0 +1,24 @@
+package quad_tile
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTileForPoint(t *testing.T) {
+	got := TileForPoint(139.734531, 35.607090)
+	want := uint64(3977143975)
+
+	if got != want {
+		t.Fatalf("want = %d, got = %d", want, got)
+	}
+}
+
+func TestTilesForArea(t *testing.T) {
+	got := TilesForArea(139.73424, 35.60433, 139.73673, 35.60707)
+	want := []uint64{3977143974, 3977143975}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("want = %v, got = %v", want, got)
+	}
+}
